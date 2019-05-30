faa3258534c4baa97b06635506c3a0b8e58f3eb0
Brian Yin <brianyin@berkeley.edu>
2019-05-30 12:45:19 -0500

    turn script into function

--- group.py
+++ group.py
@@ -3,57 +3,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import color as clr
 
+def processData(myFolder, assocTol=0.07, numHouses=4, colorsPerHouse=5, assocRange=(0, 0.2)):
+    for i in range(12):
+        myConcept = i
 
-assocTol = 0.07
-numHouses = 4
-colorsPerHouse = 5
-assocRange = [0, 0.2]
-myFolder = 'results/assocTol = 0.07 sum'
+        # get and sort data from helpers.py
+        conceptData = helpers.data[myConcept] # 1xnumColors; correlation for each of numColors colors
+        numColors = len(conceptData)
+        conceptData =[(i, conceptData[i]) for i in range(numColors)] # [(color index, association)]
+        conceptData.sort(key=lambda x: x[1], reverse=True)
+        colorIndices = [color[0] for color in conceptData]
+        colorAssoc = [color[1] for color in conceptData]
 
-for i in range(12):
-    myConcept = i
+        # get colors for plotting
+        labBarColors = [helpers.colorData[i] for i in colorIndices] # [[L, a, b]]
+        rgbBarColors = [clr.lab2rgb([[labVal]])[0][0] for labVal in labBarColors] # change lab to rgb
 
-    # get and sort data from helpers.py
-    conceptData = helpers.data[myConcept] # 1xnumColors; correlation for each of numColors colors
-    numColors = len(conceptData)
-    conceptData =[(i, conceptData[i]) for i in range(numColors)] # [(color index, association)]
-    conceptData.sort(key=lambda x: x[1], reverse=True)
-    colorIndices = [color[0] for color in conceptData]
-    colorAssoc = [color[1] for color in conceptData]
+        # colors strongly associated with myConcept
+        houseColors = []
+        index = 0
+        while len(houseColors) < numHouses:
+            houseColors.append(conceptData[index])
+            if not (conceptData[index][1] - conceptData[index + 1][1] <= assocTol):
+                houseColors = []
+            index += 1
 
-    # get colors for plotting
-    labBarColors = [helpers.colorData[i] for i in colorIndices] # [[L, a, b]]
-    rgbBarColors = [clr.lab2rgb([[labVal]])[0][0] for labVal in labBarColors] # change lab to rgb
+        """"# plot
+        y_pos = np.arange(numColors)
+        plt.bar(y_pos, colorAssoc, color=rgbBarColors)
+        plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+        plt.title(helpers.allConcepts[myConcept])
+        plt.show()"""
 
-    # colors strongly associated with myConcept
-    houseColors = []
-    index = 0
-    while len(houseColors) < numHouses:
-        houseColors.append(conceptData[index])
-        if not (conceptData[index][1] - conceptData[index + 1][1] <= assocTol):
-            houseColors = []
-        index += 1
+        # group colors
+        myGroups = helpers.groupColors(myConcept, houseColors, conceptData, assocRange, colorsPerHouse)
+        print('done')
 
-    """"# plot
-    y_pos = np.arange(numColors)
-    plt.bar(y_pos, colorAssoc, color=rgbBarColors)
-    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-    plt.title(helpers.allConcepts[myConcept])
-    plt.show()"""
+        #display colors
+        dispArray = []
+        for group in myGroups:
+            groupVec = []
+            for color in group.myColors:
+                groupVec.append(clr.lab2rgb([[color.value]])[0][0])
+            dispArray.append(groupVec)
 
-    myGroups = helpers.groupColors(myConcept, houseColors, conceptData, assocRange, colorsPerHouse)
-    print('done')
-
-    dispArray = []
-
-    for group in myGroups:
-        groupVec = []
-        for color in group.myColors:
-            groupVec.append(clr.lab2rgb([[color.value]])[0][0])
-        dispArray.append(groupVec)
-
-    plt.imshow(dispArray)
-    plt.title(helpers.allConcepts[myConcept])
-    # plt.show()
-
-    plt.savefig(myFolder + '/' + helpers.allConcepts[myConcept] + '.svg', format='svg')
+        plt.imshow(dispArray)
+        plt.title(helpers.allConcepts[myConcept])
+        plt.savefig(myFolder + '/' + helpers.allConcepts[myConcept] + '.svg', format='svg')
